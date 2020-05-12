1c101815b976ee51396d8a2ab246a48a878fac91
Antonio Neto <antoniocavalcante1910@hotmail.com>
2020-05-12 15:29:35 -0300

    path

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
 const os = require('os')
-
+const log = require('./logger')
 
 setInterval(() => {
 const { freemem, totalmem} = os
@@ -16,4 +16,6 @@ const stats = {
 }
 console.clear()
 console.log(stats)
+log(`${JSON.stringify(stats)}\n`)
+
 }, 1000)
