d8451d40f649ed4306b269819e1f91532ad3ad34
Antonio Neto <antoniocavalcante1910@hotmail.com>
2020-05-13 10:42:35 -0300

    ajust api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-const http = require("http");
-const URL = require("url");
-const fs = require("fs");
-const path = require("path");
+const http = require("http")
+const URL = require("url")
+const fs = require("fs")
+const path = require("path")
 
 const data = require("./urls.json");
 
@@ -12,26 +12,25 @@ function writeFile(cb) {
     (err) => {
       if (err) throw err;
 
-      cb(JSON.stringify({ message: "ok" }));
+      cb(JSON.stringify({ message: "ok" }))
     }
-  );
+  )
 }
 
-http
-  .createServer((req, res) => {
-    const { name, url, del } = URL.parse(req.url, true).query;
+http.createServer((req, res) => {
+    const { name, url, del } = URL.parse(req.url, true).query
     res.writeHead(200, {
       'Access-Control-Allow-Origin' : '*'
     })
 
-    if (!name || !url) return res.end(JSON.stringify(data));
+    if (!name || !url) return res.end(JSON.stringify(data))
 
     if (del) {
-      data.urls = data.urls.filter((item) => String(item.url) !== String(url));
+      data.urls = data.urls.filter((item) => String(item.url) !== String(url))
       return writeFile((message) => res.end(message))
     }
 
     data.urls.push({name, url})
     return writeFile((message) => res.end(message))
   })
-  .listen(3000, () => console.log("Api is Running"));
+  .listen(3000, () => console.log("Api is Running"))
--- public/scripts.js
+++ public/scripts.js
@@ -1,47 +1,66 @@
-const ul = document.querySelector("ul")
-const input = document.querySelector("input")
-const form = document.querySelector('form')
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+
+async function load() {
+  const res = await fetch("http://localhost:3000/").then((data) => data.json())
+  console.log(res);
+  res.urls.map(({name, url}) => addElement({name, url}))
+}
+
+async function deleted() {
+    const res = await fetch("http://localhost:3000/del").then((data) => data.json())
+    // console.log(res);
+    res.urls.map(({name, url}) => delete({name, url}))
+  }
+
+//    function salved() {
+//     const res = fetch("http://localhost:3000/").then((data) => data.json())
+//     console.log(res);
+//     res.urls.map(({name, url}) => push({name, url}))
+//   }
 
 
+load();
+
+// saved();
 function addElement({ name, url }) {
-    const li = document.createElement('li')
-    const a = document.createElement("a")
-    const trash = document.createElement("span")
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const trash = document.createElement("span");
+//   salved();
 
-    a.href = url
-    a.innerHTML = name
-    a.target = "_blank"
+  a.href = url;
+  a.innerHTML = name;
+  a.target = "_blank";
 
-    trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+  trash.innerHTML = "x";
+  trash.onclick = () => removeElement(trash);
 
-    li.append(a)
-    li.append(trash)
-    ul.append(li)
+  li.append(a);
+  li.append(trash);
+  ul.append(li);
 }
 
 function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
-        el.parentNode.remove()
+  if (confirm("Tem certeza que deseja deletar?")) el.parentNode.remove();
+  deleted();
 }
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let { value } = input
+  let { value } = input;
 
-    if (!value) 
-        return alert('Preencha o campo')
+  if (!value) return alert("Preencha o campo");
 
-    const [name, url] = value.split(",")
+  const [name, url] = value.split(",");
 
-    if (!url) 
-        return alert('formate o texto da maneira correta')
+  if (!url) return alert("formate o texto da maneira correta");
 
-    if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta")
+  if (!/^http/.test(url)) return alert("Digite a url da maneira correta");
 
-    addElement({ name, url })
+  addElement({ name, url });
 
-    input.value = ""
-})
+  input.value = "";
+});
