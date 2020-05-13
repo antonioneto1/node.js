0c447c0ac91018673e08ce0d6faef61f4b80ac9d
Antonio Neto <antoniocavalcante1910@hotmail.com>
2020-05-13 07:26:34 -0300

    inclusão de paginas html, css e js, nodemon e rodando o servidor

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 const path = require('path')
 
 http.createServer((req, res) => {
- if(req.url === '/'){
+  const file = req.url === '/' ? 'index.html' : req.url
+  const filePath = path.join(__dirname, 'public', file)
+  const extname = path.extname(filePath)
+  
+  const allowedFileTypes = ['.html', '.css', '.js']
+  const allowed = allowedFileTypes.find(item => item == extname)
+
+  if(!allowed) return
+
    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
+    filePath,
       (err, content) => {
         if(err) throw err
 
         res.end(content)
       }
    )
- }
-
 }).listen(5000,() => console.log('Server is Running'))
